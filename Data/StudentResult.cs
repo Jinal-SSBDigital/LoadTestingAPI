5cfe5c07038c7d88d20f951c09947f430e0a5189
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-05 17:18:44 +0530

    Add StudentResult model
    Introduce StudentResult POCO in Data namespace to represent a student's exam details, including roll number, names, college/faculty, total marks, division, and a list of SubjectResult entries. This adds the domain model used to return or persist student result data (depends on SubjectResult type).

---
+++ Data/StudentResult.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoadTestingAPI.Data
+{
+    public class StudentResult
+    {
+        public int RollNumber { get; set; }
+        public string StudentFullName { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string Faculty { get; set; }
+        public string CollegeName { get; set; }
+        public int TotalMarks { get; set; }
+        public string Division { get; set; }
+
+        public List<SubjectResult> Subjects { get; set; }
+    }
+}
