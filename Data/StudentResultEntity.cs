9cf834a6acaaa310cde7f77f7cbe2110eca3f88c
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-05 17:17:58 +0530

    Add StudentResultEntity model
    Introduce StudentResultEntity in LoadTestingAPI.Data to represent student result data. The POCO includes personal details (RollNumber, StudentFullName, FatherName, MotherName), institution info (Faculty, CollegeName), overall academic fields (TotalMarks, Division) and subject-level fields (SubjectPaperName, ObtainedMarks, SubjectTotal).

---
+++ Data/StudentResultEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace LoadTestingAPI.Data
+{
+    public class StudentResultEntity
+    {
+        public int RollNumber { get; set; }
+        public string StudentFullName { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string Faculty { get; set; }
+        public string CollegeName { get; set; }
+        public int TotalMarks { get; set; }
+        public string Division { get; set; }
+
+        public string SubjectPaperName { get; set; }
+        public int ObtainedMarks { get; set; }
+        public string SubjectTotal { get; set; }
+    }
+}
