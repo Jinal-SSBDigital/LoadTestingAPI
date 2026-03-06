f74b8214676c7202b07668290c3f25121df5ca1a
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-06 13:54:02 +0530

    Make StudentResultEntity properties nullable
    Convert most StudentResultEntity properties to nullable types to support optional/missing data. RollNumber was changed from int to string to allow non-numeric IDs; RegistrationNo (string?) was added. Numeric mark fields (TotalMarks, ObtainedMarks) are now nullable ints and all string fields were made nullable.

--- Data/StudentResultEntity.cs
+++ Data/StudentResultEntity.cs
@@ -2,17 +2,18 @@
 {
     public class StudentResultEntity
     {
-        public int RollNumber { get; set; }
-        public string StudentFullName { get; set; }
-        public string FatherName { get; set; }
-        public string MotherName { get; set; }
-        public string Faculty { get; set; }
-        public string CollegeName { get; set; }
-        public int TotalMarks { get; set; }
-        public string Division { get; set; }
+        public string? RollNumber { get; set; }
+        public string? RegistrationNo { get; set; }
+        public string? StudentFullName { get; set; }
+        public string? FatherName { get; set; }
+        public string? MotherName { get; set; }
+        public string? Faculty { get; set; }
+        public string? CollegeName { get; set; }
+        public int?   TotalMarks { get; set; }
+        public string? Division { get; set; }
 
-        public string SubjectPaperName { get; set; }
-        public int ObtainedMarks { get; set; }
-        public string SubjectTotal { get; set; }
+        public string? SubjectPaperName { get; set; }
+        public int? ObtainedMarks { get; set; }
+        public string? SubjectTotal { get; set; }
     }
 }
